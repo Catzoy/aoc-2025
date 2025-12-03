117661f8cef09b9579c162524d9c9049beb328ad
Catzoy <demchenko20021@gmail.com>
2025-12-03 10:15:54 +0200

    Day 3: Part 2

--- src/main/kotlin/d03/common.kt
+++ src/main/kotlin/d03/common.kt
@@ -1,20 +1,26 @@
 package d03
 
-fun CharArray.findMostJoltage(): Int {
-    var c1 = '0'
-    var c2 = '0'
+fun CharArray.findMostJoltage(length: Int): Long {
+    var joltage = List(length) { '0' }
+    var relative = List(length) { -1 }
     for (position in indices) {
         val char = get(position)
-        when {
-            char > c1 && position != lastIndex -> {
-                c1 = char
-                c2 = get(position + 1)
-            }
-
-            char > c2 -> {
-                c2 = char
-            }
+        val indexMax = joltage.withIndex()
+            .asSequence()
+            // number is greater than some of existing
+            .filter { (_, cx) -> char > cx }
+            // after the position there is enough values for the rest of the battery
+            .filter { (i, _) -> (lastIndex - position >= joltage.lastIndex - i) }
+            // this position was not previously used
+            .filterNot { (i, _) -> relative.take(i + 1).contains(position) }
+            .firstOrNull()
+            ?.index
+        if (indexMax != null) {
+            val retained = 0 until indexMax
+            val updated = position..position + (joltage.lastIndex - indexMax)
+            joltage = joltage.slice(retained) + slice(updated)
+            relative = relative.slice(retained) + updated.toList()
         }
     }
-    return "$c1$c2".toInt()
+    return joltage.joinToString(separator = "").toLong()
 }
--- src/main/kotlin/d03/p2.kt
+++ src/main/kotlin/d03/p2.kt
@@ -6,7 +6,7 @@ fun main() {
     val maxJoltage = readAsInput("03")
         .asSequence()
         .map { str -> str.toCharArray() }
-        .map { arr -> arr.findMostJoltage(length = 2) }
+        .map { arr -> arr.findMostJoltage(length = 12) }
         .onEach { println("Joltage: $it") }
         .sum()
     println(maxJoltage)
